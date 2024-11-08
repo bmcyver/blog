b65fbd4c83e07fa413322f4bc0cb31ee22363173
bmcyver <bm.cyver@gmail.com>
2024-11-08 17:26:12 +0900

    Add remark-github-blockquote-alert for enhanced markdown support and implement reading time calculation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
+import { remarkReadingTime } from './src/plugins/remarkReadingTime'
 import remarkUnwrapImages from 'remark-unwrap-images';
 import rehypeExternalLinks from 'rehype-external-links';
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import expressiveCode from 'astro-expressive-code';
+import { remarkAlert } from "remark-github-blockquote-alert";
 import { expressiveCodeOptions } from './src/site.config';
 import icon from 'astro-icon';
 import node from "@astrojs/node";
@@ -20,7 +21,7 @@ export default defineConfig({
     applyBaseStyles: false
   }), sitemap(), mdx(), icon()],
   markdown: {
-    remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkMath],
+    remarkPlugins: [remarkUnwrapImages, remarkAlert, remarkReadingTime, remarkMath],
     rehypePlugins: [[rehypeExternalLinks, {
       target: '_blank',
       rel: ['nofollow, noopener, noreferrer']
